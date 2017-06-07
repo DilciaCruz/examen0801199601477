055b1cb683790a3a71612736c20b57874cdab99e
Unknown <dilcia_salgado1995@hotmail.com>
2017-06-06 19:30:52 -0600

    Suma de dos enteros post
    Datos generales en un get

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,30 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/0801199601477', function(req,res,next){
+  var datos = [
+                {"Nombre":"Dilcia Rosibel Cruz Salgado","Cuenta":"0801199601477","Correo Electronico":"dilcia_salgado1995@hotmail.com"}
+              ];
+  res.json(datos);
+
+});
+
+router.get('/sumar', function(req,res,next){
+  res.render('suma',{"txtentero1":"","txtentero2":""});
+});
+
+
+var resultado = [];
+
+router.post('/sumar',function(req,res,next){
+  console.log(req.body);
+    resultado.push(parseInt(req.body.txtentero1)+parseInt(req.body.txtentero2));
+    var msgs = resultado;
+    var rtObject = {}; 
+    rtObject.txtentero1 = req.body.txtentero1;
+    rtObject.txtentero2 = req.body.txtentero2;
+    rtObject.msg= msgs;
+  res.render('suma', rtObject);
+});
 
 module.exports = router;
